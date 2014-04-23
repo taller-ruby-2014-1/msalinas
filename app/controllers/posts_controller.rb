04954ba44611b2a8acee24fd8b431c6765dd8f48
StAx <marco.salinas.12@sansano.usm.cl>
2014-04-23 17:17:03 -0300

    ta bonito ruby

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,12 @@
 class PostsController < ApplicationController
 	def new
+		@post = Post.new
 	end
 	def create
 		render text: params[:post].inspect
 	end
 
-
-	def show
+def show
   @post = Post.find(params[:id])
 end
 
@@ -15,12 +15,37 @@ def index
 end
 
 def create
-  @post = Post.new(post_params)
+  @post = Post.new(params[:post].permit(:title, :text))
  
-  @post.save
-  redirect_to @post
+  if @post.save
+ 	redirect_to @post
+ else
+ 	render 'new'
+ end
 end
+
+def edit
+	@post = Post.find(params[:id])
+  #{params[:id]} llevan a todos los post al 1
+end 
+
+def update
+  @post = Post.find(params[:id])
  
+  if @post.update(params[:post].permit(:title, :text))
+    redirect_to @post
+  else
+    render 'edit'
+  end
+end
+
+def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+ 
+  redirect_to posts_path
+end
+
 private
   def post_params
     params.require(:post).permit(:title, :text)
