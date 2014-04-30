3121b9ad2a6bb5bf70c0a7a3fa84992d7f97841d
StAx <marco.salinas.12@sansano.usm.cl>
2014-04-30 16:57:49 -0400

    yupi

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,55 +1,55 @@
 class PostsController < ApplicationController
-	def new
-		@post = Post.new
-	end
-	def create
-		render text: params[:post].inspect
-	end
-
-def show
-  @post = Post.find(params[:id])
-end
-
-def index
-  @posts = Post.all
-end
-
-def create
-  @post = Post.new(params[:post].permit(:title, :text))
- 
-  if @post.save
- 	redirect_to @post
- else
- 	render 'new'
- end
-end
-
-def edit
-	@post = Post.find(params[:id])
-  #{params[:id]} llevan a todos los post al 1
-end 
-
-def update
-  @post = Post.find(params[:id])
- 
-  if @post.update(params[:post].permit(:title, :text))
-    redirect_to @post
-  else
-    render 'edit'
+  def new
+  	@post = Post.new
   end
-end
-
-def destroy
-  @post = Post.find(params[:id])
-  @post.destroy
- 
-  redirect_to posts_path
-end
-
-private
-  def post_params
-    params.require(:post).permit(:title, :text)
+  def create
+  	render text: params[:post].inspect
   end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  def create
+    @post = Post.new(params[:post].permit(:title, :text))
+   
+    if @post.save
+   	redirect_to @post
+   else
+   	render 'new'
+   end
+  end
+
+  def edit
+  	@post = Post.find(params[:id])
+    #{params[:id]} llevan a todos los post al 1
+  end 
+
+  def update
+    @post = Post.find(params[:id])
+   
+    if @post.update(params[:post].permit(:title, :text))
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+   
+    redirect_to posts_path
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :text)
+    end
 	
 end
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
 	validates :title, presence: true,
 			   length: {minimum: 5}
-	has_many :comments
+	has_many :comments, dependent: :destroy
+
 end
